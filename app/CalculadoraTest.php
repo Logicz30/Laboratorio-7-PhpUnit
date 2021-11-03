cd052a90b08b455858d3f849e5ff96e73a9a5c52
Sebastian Samaniego <ssamaniego30@gmail.com>
2021-11-03 18:54:25 -0500

    Revert "Lab8"
    This reverts commit 4f4f0b9ccabda56a4799ae5f0469169da34ca70f.

--- app/CalculadoraTest.php
+++ app/CalculadoraTest.php
@@ -22,7 +22,6 @@ class CalculadoraTest extends TestCase
     {
         $calculadora = new Calculadora();
         //$this->assertEquals("6",  $calculadora->sumar(3,3));
-        //$this->assertSame($resultado_esperado, $calculadora->sumar($numero1,$numero2));
         $this->assertEquals($resultado_esperado, $calculadora->sumar($numero1,$numero2));
     }
 
@@ -95,9 +94,8 @@ class CalculadoraTest extends TestCase
     public function testGenerarArreglo()
     {
         $calculadora = new Calculadora();
-        //$this->assertContains(1, $calculadora->generarArreglo(),"Error en generar arreglo");
+        //$this->assertContains(7, $calculadora->generarArreglo());
         //$this->assertCount(5, $calculadora->generarArreglo());
-        //$this->assertEmpty( $calculadora->generarArreglo());
         $this->assertNotEmpty( $calculadora->generarArreglo());
     }
 }
